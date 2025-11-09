28a4988cddcef8d9a091741b67bccf59ec0d8569
Aaqil khan <aaqilpro99@gmail.com>
2025-11-10 03:31:41 +0530

    Implemented landing page with rtk query

--- store/store.ts
+++ store/store.ts
@@ -1,15 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { profileApi } from './profileApi'
 import { menuApi } from './menuApi'
+import { offersApi } from './offersApi'
 import authReducer from './authSlice'
 
 export const store = configureStore({
   reducer: {
     [profileApi.reducerPath]: profileApi.reducer,
     [menuApi.reducerPath]: menuApi.reducer,
+    [offersApi.reducerPath]: offersApi.reducer,
     auth: authReducer,
   },
-  middleware: (getDefault) => getDefault().concat(profileApi.middleware, menuApi.middleware)
+  middleware: (getDefault) => getDefault().concat(profileApi.middleware, menuApi.middleware, offersApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
